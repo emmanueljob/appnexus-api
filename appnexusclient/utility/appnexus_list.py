877fbd6e7c745f43f032ca911c12ba88feeeed31
Emmanuel Job <emmanuel.job@accuenmedia.com>
2015-02-10 19:20:09 +0000

    new files for lists

---
+++ appnexusclient/utility/appnexus_list.py
@@ -0,0 +1,13 @@
+
+
+class AppnexusList(list):
+    count = 0
+
+    def count(self):
+        return self.count
+
+    def set_count(self, count):
+        self.count = count
+
+    def __len__(self):
+        return self.count()
